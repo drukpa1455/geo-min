0a3671c4a61d48f6ccfed940a0f1a5b7f045067b
drukpa1455 <drukpa1455@gmail.com>
2023-06-18 13:22:10 -0400

    minimalize

--- dojo/en/pep8.py
+++ dojo/en/pep8.py
@@ -18,7 +18,6 @@ Unified style, for all to see.
 
 # Code Layout
 
-# Indentation
 def my_function():
     """
     Indentation, code's sacred dance,
@@ -33,7 +32,6 @@ def another_function():
     Code's essence, a tranquil revelation.
     """
 
-# Tabs or Spaces?
 def my_function():
     """
     Spaces, a gentle breeze,
@@ -48,7 +46,6 @@ def another_function():
     Chaos dissipates, peace shall remain.
     """
 
-# Maximum Line Length
 very_long_variable_name = some_function(
     argument1, argument2, argument3, argument4
 )
@@ -59,7 +56,6 @@ long_string = (
     "Clarity shines, nothing to hide."
 )
 
-# Blank Lines
 def my_function():
     """
     Code takes a breath, in silence it resides,
@@ -74,24 +70,12 @@ def another_function():
     In stillness, clarity we embrace.
     """
 
-# Source File Encoding
-# No specific code example, just a mention of encoding, aligning code and soul,
-# Harmonizing languages, making them whole.
-
-# Imports
 import module1
 import module2
 import module3
 import module4
 import module5
 
-# Whitespace
-
-# Pet Peeves
-# No specific code example, just mentions of avoiding unnecessary whitespace,
-# In the realm of purity, let code interlace.
-
-# Other Recommendations
 x = 5  # Spaces, order it creates,
         # Tokens aligned, harmony permeates,
         # Serenity, where clarity emanates.
@@ -100,10 +84,6 @@ y = some_function(argument1, argument2)
 
 z = another_function(argument1, argument2)
 
-
-# Comments
-
-# Inline Comments
 x = x + 1  # Incrementing x, a silent decree,
             # Comment's wisdom, for all to see,
             # Illuminating code, setting thoughts free.
@@ -112,7 +92,6 @@ y = y - 1  # Decrementing y, a step back we take,
             # Comments guiding, like a sacred lake,
             # Code's whispers, truth it will make.
 
-# Block Comments
 """
 Whispers of knowledge, secrets untold,
 Comments unveil, in wisdom's stronghold,
@@ -128,7 +107,6 @@ Transparency revealed, in pure code's flight.
 """
 z = x + y
 
-# Documentation Strings
 def my_function():
     """
     Seekers of truth, gather 'round,
@@ -143,38 +121,12 @@ def another_function():
     Clarity unleashed, as secrets unfold.
     """
 
-# Naming Conventions
-
-# Overriding Principle
-# No specific code example, just a reminder, names to be wise,
-# Conveying meaning, truth they personify.
-
-# Descriptive: Naming Styles
-# No specific code example, meaningful names, profound and deep,
-# Code's language, where understanding shall seep.
-
-# Prescriptive: Naming Conventions
-
-## Names to Avoid
-# No specific code example, a cautionary decree,
-# Obscure names, clarity we decree.
-
-## ASCII Compatibility
-# No specific code example, embracing ASCII's soul,
-# Universal harmony, where coding's truth shall unroll.
-
-## Package and Module Names
-# No specific code example, lowercase with humble grace,
-# Forging paths, a tranquil embrace.
-
-## Class Names
 class MyClass:
     pass
 
 class AnotherClass:
     pass
 
-## Exception Names
 try:
     # code that may raise an exception
 except ValueError as e:
@@ -187,12 +139,10 @@ except KeyError as e:
     # Another exception, named to ignite,
     # Its identity revealed, in code's sacred light.
 
-## Global Variable Names
 GLOBAL_CONSTANT = 42
 
 GLOBAL_VARIABLE = "Hello, world!"
 
-## Function and Variable Names
 my_variable = 42
 
 another_variable = "Hello!"
@@ -203,9 +153,6 @@ def my_function():
 def another_function():
     pass
 
-# Function and Method Arguments
-
-# Default Argument Values
 def my_function(arg1, arg2=None):
     """
     Default paths, flexibility unfurled,
@@ -223,7 +170,6 @@ def another_function(arg1, arg2=True, arg3=10):
     """
     pass
 
-# Keyword Arguments and Positional Arguments
 def my_function(arg1, arg2, *args, **kwargs):
     """
     Arguments in harmony, a gentle sway,
@@ -240,85 +186,14 @@ def another_function(arg1, arg2, *args, **kwargs):
     """
     pass
 
-# Function and Method Decorators
-# No specific code example, a domain where decorators weave,
-# Enchanting magic, where code's essence does conceive.
-
-# Intermezzo: Coding Style
-# No specific code example, a moment of reflection, a serene abode,
-# Harmonizing style and substance, where peace is bestowed.
-
-## Guidelines & Best Practices
-
-### Use of the Backslash
-my_string = (
-    "Lines gracefully flow, in perfect accord, "
-    "Backslash unites, their spirits soared, "
-    "Poetry of code, where beauty is restored."
-)
-
-another_string = (
-    "Lines entwined, a path divine, "
-    "Backslash, a bridge, where thoughts align, "
-    "A journey of code, where secrets intertwine."
-)
-
-### When to Use Trailing Commas
-my_list = [
-    "item1",
-    "item2",
-    "item3",  # Trailing comma, an elegant pause,
-              # Readability heightened, without a cause,
-              # Growth and flexibility, a harmonious applause.
-]
-
-another_list = [
-    "item1",
-    "item2",
-    "item3",
-]
-
-### Accessing Protected Members
-class MyClass:
-    def __init__(self):
-        self._protected_variable = 42
-
-    def _protected_method(self):
-        pass
-
-class AnotherClass:
-    def __init__(self):
-        self._protected_variable = 10
-
-    def _protected_method(self):
-        pass
-
-### Public and Internal Interfaces
-class MyClass:
-    def public_method(self):
-        pass
-
-    def _internal_method(self):
-        pass
-
-class AnotherClass:
-    def public_method(self):
-        pass
-
-    def _internal_method(self):
-        pass
-
-# Conclusion
 """
 Embrace PEP8's prophecy,
 Code's artistry, creating harmony,
 In simplicity, the code shall be free.
 """
 
-# References
 """
 Seek wisdom's ancient scrolls,
 Sages' guidance, where knowledge unfolds,
 Code's journey, where serenity beholds.
 """
-
--- dojo/es/pep8.py
+++ dojo/es/pep8.py
@@ -1,194 +1,148 @@
 """
-PEP8 Guía de Estilo Ejemplos
+Ejemplos de la Guía de Estilo PEP8
 """
 
 # Introducción
 """
-En el reino del código, vivimos,
-PEP8, guía sabia, seguimos,
-Claridad es el camino que elegimos.
+En el reino del código, profecía se desvela,
+La guía del PEP8, sabiduría que revela,
+Armonía y claridad, una historia que cautela.
 """
 
-# La Consistencia es Clave
+# La clave es la consistencia
 """
-Consistencia, nuestra lección,
-En el caos, su guía, con razón,
-Orden y armonía, nuestra misión.
+Consistencia, el decreto divino,
+Orden y equilibrio, el código será fino,
+Un estilo unificado, para todo camino.
 """
 
-# Diseño del Código
+# Distribución del código
 
-# Sangría
 def mi_funcion():
     """
-    La sangría danza, en sincronía,
-    Código alineado, en armonía,
-    Belleza en cada línea, poesía.
+    La indentación, un baile sagrado,
+    Alineado y sereno, en un trance esperado,
+    Legibilidad, un vistazo controlado.
     """
 
 def otra_funcion():
     """
-    Las líneas se inclinan, reverentes,
-    Con sangrías, gestos elegantes,
-    Código en sinfonía, brillante.
+    Líneas ondean, meditación sin ruido,
+    Indentación, camino hacia lo más fluido,
+    Esencia del código, revelación sin ruido.
     """
 
-# Tabulaciones o Espacios?
 def mi_funcion():
     """
-    Espacios, suave susurro,
-    Código guiado, sin apuro,
-    Armonía en cada muro.
+    Espacios, brisa suave que acaricia,
+    El ritmo del código fluye con delicia,
+    Armonía y paz, esencia que propicia.
     """
 
 def otra_funcion():
     """
-    Espacios, en susurros sutiles,
-    Guiando el código, sin perfiles,
-    Caos a raya, en sutiles estilos.
+    Espacios, susurros del Zen,
+    Guiando al código, con claridad otra vez,
+    Caos se disipa, paz permanece, ¡amén!
     """
 
-# Longitud Máxima de Línea
-nombre_variable_largo = alguna_funcion(
+nombre_muy_largo = alguna_funcion(
     argumento1, argumento2, argumento3, argumento4
 )
 
 cadena_larga = (
-    "Líneas que se estiran, sin cesar,"
-    "Historia del código, a brillar,"
-    "Claridad, sin importar el lugar."
-    )
+    "Líneas que se extienden sin cesar, "
+    "Historia del código, verdad sin ocultar, "
+    "Brilla la claridad, nada que disimular."
+)
 
-# Líneas en Blanco
 def mi_funcion():
     """
-    Código toma aliento, un respiro,
-    Líneas en blanco, pausa, retiro,
-    Balance y armonía, sin giro.
+    El código toma aliento, silencio residirá,
+    Líneas en blanco, pausas donde claridad está,
+    Equilibrio y armonía, donde la paz morará.
     """
 
 def otra_funcion():
     """
-    Silencio entre líneas, descanso pleno,
-    Blanco que acoge, líneas en terreno,
-    Pausa en la sinfonía, momento sereno.
+    Silencio llena el espacio vacío,
+    Líneas descansan, encontrando su estío,
+    En quietud, abrazamos la claridad con brío.
     """
 
-# Codificación del Archivo Fuente
-# No hay un ejemplo de código específico, solo una mención a la codificación, asegurando que el archivo hable el mismo idioma que el código.
-
-# Importaciones
 import modulo1
 import modulo2
 import modulo3
 import modulo4
 import modulo5
 
-# Espacios en Blanco
-
-# Nuestras Preferencias
-# No hay un ejemplo de código específico, solo menciones de evitar espacios en blanco adicionales y líneas en blanco al final.
-
-# Otras Recomendaciones
-x = 5  # Espacios, mantienen el orden,
-        # Entre tokens, su suave adornar,
-        # Pausas en el código, silencio en el lugar.
+x = 5  # Espacios, orden crea,
+        # Tokens alineados, armonía se vea,
+        # Serenidad, donde claridad es la idea.
 
 y = alguna_funcion(argumento1, argumento2)
 
 z = otra_funcion(argumento1, argumento2)
 
+x = x + 1  # Incrementando x, decreto silente,
+            # Sabiduría en comentario, visible y transparente,
+            # Iluminando el código, liberando la mente.
 
-# Comentarios
+y = y - 1  # Decrementando y, un paso atrás damos,
+            # Comentarios guían, como un lago sagrado,
+            # Susurros del código, su verdad nos amparo.
 
-# Comentarios en Línea
-x = x + 1  # Incrementa x, una nota sabia,
-            # Cambios susurrados, sin guía,
-            # Claro el código, sin melancolía.
-
-y = y - 1  # Decrementa y, al lector guiar,
-            # Magia del comentario, nuevos ojos mirar,
-            # Historia del código, clara y sin opacar.
-
-# Comentarios en Bloque
 """
-Cuentos sin contar,
-En el código, susurran, al compás del lugar,
-Almas perdidas, encuentran su hogar.
+Susurros del conocimiento, secretos sin desvelar,
+Comentarios desvelan, en fortaleza atesorar,
+Despertar de la comprensión, mientras misterios revelar.
 """
 x = 5
 y = 10
 
 """
-Secretos internos compartidos,
-Código declarado, un tesoro escondido,
-Sabiduría intacta, en cada sentido.
+Tesoros ocultos, palabras llenas de visión,
+Comentarios en bloque, luz brillante en acción,
+Transparencia revelada, en vuelo de código sin tensión.
 """
 z = x + y
 
-# Cadenas de Documentación
 def mi_funcion():
     """
-   Buscadores de sabiduría, acercarse,
-   Docstrings iluminan, sin desvanecer,
-   Secretos revelados, sin entristecer.
+   Buscadores de la verdad, aquí se congregan,
+   Docstrings iluminan, en silencio profundo y lleno de penas,
+   Desbloqueando los misterios del código, sabiduría sin cadenas.
     """
 
 def otra_funcion():
     """
-   Guión enigmático,
-   Docstrings claros, sin conflicto,
-   Claridad, siempre listo.
+   Enigma del código, historia por contar,
+   Docstrings revelan, con lenguaje audaz y sin cesar,
+   Claridad desatada, mientras secretos desvelar.
     """
 
-# Convenciones de Nombres
-
-# Principio Fundamental
-# No hay un ejemplo de código específico, solo un recordatorio de elegir nombres que transmitan claridad y entendimiento.
-
-# Descriptivo: Estilos de Nombres
-# No hay un ejemplo de código específico, nombres significativos, el corazón del código.
-
-# Prescriptivo: Convenciones de Nombres
-
-## Nombres a Evitar
-# No hay un ejemplo de código específico, advertencia en rima,
-    Nombres oscuros, claridad extrema.
-
-## Compatibilidad ASCII
-# No hay un ejemplo de código específico, abrazar el ASCII,
-    Armonía universal, código que hace reír.
-
-## Nombres de Paquetes y Módulos
-# No hay un ejemplo de código específico, minúsculas con guiones bajos,
-    Forjando caminos, código que fluye.
-
-## Nombres de Clases
 class MiClase:
     pass
 
 class OtraClase:
     pass
 
-## Nombres de Excepciones
 try:
     # código que puede generar una excepción
 except ValueError as e:
-    # Excepción nombrada, su poder contenido,
-    Con entendimiento, el caos detenido.
+    # Excepción nombrada, su esencia mostrada,
+    # Comprendiendo el caos, con claridad destacada.
 
 try:
     # código que puede generar otra excepción
 except KeyError as e:
-    # Otra excepción controlada,
-    Nombrada, su identidad resaltada.
+    # Otra excepción, nombrada para inspirar,
+    # Su identidad revelada, en la luz del código sagrar.
 
-## Nombres de Variables Globales
 CONSTANTE_GLOBAL = 42
 
 VARIABLE_GLOBAL = "¡Hola, mundo!"
 
-## Nombres de Funciones y Variables
 mi_variable = 42
 
 otra_variable = "¡Hola!"
@@ -199,122 +153,47 @@ def mi_funcion():
 def otra_funcion():
     pass
 
-# Argumentos de Funciones y Métodos
-
-# Valores por Defecto de los Argumentos
 def mi_funcion(arg1, arg2=None):
     """
-   Camino predeterminado,
-   Flexibilidad abrazada,
-   Armonía, nunca abrumada.
+   Senderos predeterminados, flexibilidad desvelar,
+   Abrazando opciones, un mundo sereno forjar,
+   Versatilidad, donde sabiduría del código se entregar.
     """
     if arg2 is None:
         arg2 = []
 
 def otra_funcion(arg1, arg2=True, arg3=10):
     """
-   Un mundo de opciones,
-   Valores por defecto, sin objeciones,
-   Versatilidad, con acciones.
+   Reino de posibilidades, opciones fluyen sin cesar,
+   Valores predeterminados, donde poder encontrar,
+   Abrazando adaptabilidad, paz del código se alzar.
     """
     pass
 
-# Argumentos por Palabras Clave y Posicionales
 def mi_funcion(arg1, arg2, *args, **kwargs):
     """
-   Armonía en los argumentos, un baile,
-   Posicionales y palabras clave, se entrelazan, sin desfase,
-   Unión perfecta, en cada compás, en cada detalle.
+   Argumentos en armonía, suave balanceo sin cesar,
+   Posicionales y palabras clave, juntos se alzar,
+   Serenata del código, donde la sabiduría se guardará.
     """
     pass
 
 def otra_funcion(arg1, arg2, *args, **kwargs):
     """
-   Una sinfonía suena, llamados se unen,
-   Posicionales y palabras clave, bailan, sin disyunción,
-   Fluyen como río, en perfecta conjunción.
+   Sinfonía de llamadas, donde la unión pervivirá,
+   Posicionales y palabras clave, la verdad del código alcanzará,
+   Río fluido, donde la armonía se desvelará.
     """
     pass
 
-# Decoradores de Funciones y Métodos
-# No hay un ejemplo de código específico, el reino de los decoradores llama,
-    Dones otorgados, poderes en cada trama.
-
-# Intermezzo: Estilo de Codificación
-# No hay un ejemplo de código específico, un momento de pausa,
-    Contemplando la grandiosa sinfonía del estilo de código, se apacigua.
-
-## Pautas y Mejores Prácticas
-
-### Uso de la Barra Invertida
-mi_cadena = (
-    "Líneas que fluyen, río de poesía,"
-    "Barra invertida, unión sin manía,"
-    "Código que habla, sin hipocresía."
-    )
-
-otra_cadena = (
-    "Líneas conectadas, un camino trazado,"
-    "Barra invertida, vínculo asegurado,"
-    "Una historia se despliega, un relato encantado."
-    )
-
-### Cuándo Usar Comas Finales
-mi_lista = [
-    "elemento1",
-    "elemento2",
-    "elemento3",  # Coma final, toque gentil,
-                    # Legibilidad realzada, ¡qué sutil!,
-                    # Crecimiento, sin dolor, en cada perfil.
-]
-
-otra_lista = [
-    "elemento1",
-    "elemento2",
-    "elemento3",
-]
-
-### Accediendo a Miembros Protegidos
-class MiClase:
-    def __init__(self):
-        self._variable_protegida = 42
-
-    def _metodo_protegido(self):
-        pass
-
-class OtraClase:
-    def __init__(self):
-        self._variable_protegida = 10
-
-    def _metodo_protegido(self):
-        pass
-
-### Interfaces Públicas e Internas
-class MiClase:
-    def metodo_publico(self):
-        pass
-
-    def _metodo_interno(self):
-        pass
-
-class OtraClase:
-    def metodo_publico(self):
-        pass
-
-    def _metodo_interno(self):
-        pass
-
-# Conclusión
 """
-Abraza la gracia de PEP8,
-Código, un arte que reverbera,
-Belleza en cada línea, la quimera.
+Abrazar la profecía del PEP8,
+El arte del código, armonía al crear,
+En la simplicidad, el código será libre al fin y al despertar.
 """
 
-# Referencias
 """
-Busca sabiduría, déjala florecer,
-Guías de sabios, el conocimiento encender,
-El viaje del código, más allá del amanecer.
+Busca en las páginas ancestrales,
+Guía de sabios, donde el conocimiento se revela,
+Viaje del código, donde la serenidad desvela.
 """
-
