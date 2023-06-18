2ad80fefb7bd6770dfedeed805dbfe756164da94
drukpa1455 <drukpa1455@gmail.com>
2023-06-17 20:21:30 -0400

    cleanup

--- pep8zen_en.py
+++ pep8zen_en.py
@@ -4,16 +4,16 @@ PEP8 Style Guide Examples
 
 # Introduction
 """
-In code's sacred realm,
-PEP8's guide, a tranquil helm,
-Clarity takes the helm.
+In the realm of code, prophecy unfolds,
+PEP8's guide, wisdom it beholds,
+Harmony and clarity, a tale untold.
 """
 
 # Consistency is Key
 """
-Consistency's plea,
-Guiding us to harmony,
-Chaos we must flee.
+Consistency, the divine decree,
+Order and balance, the code shall decree,
+Unified style, for all to see.
 """
 
 # Code Layout
@@ -21,31 +21,31 @@ Chaos we must flee.
 # Indentation
 def my_function():
     """
-    Indentation dance,
-    Aligned in elegant trance,
-    Harmony's true stance.
+    Indentation, code's sacred dance,
+    Aligned and poised, in a graceful trance,
+    Readability, a mindful glance.
     """
 
 def another_function():
     """
-    Lines gently bow,
-    Indents, a graceful vow,
-    A symphony, here and now.
+    Lines gracefully sway, in silent meditation,
+    Indentation, a path to enlightenment's elevation,
+    Code's essence, a tranquil revelation.
     """
 
 # Tabs or Spaces?
 def my_function():
     """
-    Spaces reign supreme,
-    Code's rhythm, a gentle theme,
-    Harmony, like a dream.
+    Spaces, a gentle breeze,
+    Code's rhythm, flowing with ease,
+    Harmony and peace, it decrees.
     """
 
 def another_function():
     """
-    Spaces, whispers so soft,
-    Guiding code, aloft,
-    Chaos they set aloft.
+    Spaces, whispers of Zen,
+    Guiding code, with clarity, again and again,
+    Chaos dissipates, peace shall remain.
     """
 
 # Maximum Line Length
@@ -54,28 +54,29 @@ very_long_variable_name = some_function(
 )
 
 long_string = (
-    "Lines stretch, near and far, "
-    "Code's story, like a shining star, "
-    "Clarity, no matter where you are."
-    )
+    "Lines stretching far and wide, "
+    "Code's story, truth we confide, "
+    "Clarity shines, nothing to hide."
+)
 
 # Blank Lines
 def my_function():
     """
-    Code takes a breath,
-    Blank lines, peacefulness, no death,
-    Balance, like nature's breadth.
+    Code takes a breath, in silence it resides,
+    Blank lines, pauses where clarity presides,
+    Balance and harmony, where peace abides.
     """
 
 def another_function():
     """
-    Silence in the space,
-    Lines rest, a peaceful embrace,
-    A pause, a moment's grace.
+    Silence fills the empty space,
+    Lines resting, finding their place,
+    In stillness, clarity we embrace.
     """
 
 # Source File Encoding
-# No specific code example, just a mention of encoding, ensuring the file speaks the same language as your code.
+# No specific code example, just a mention of encoding, aligning code and soul,
+# Harmonizing languages, making them whole.
 
 # Imports
 import module1
@@ -87,12 +88,13 @@ import module5
 # Whitespace
 
 # Pet Peeves
-# No specific code example, just mentions of avoiding extra whitespace and trailing blank lines.
+# No specific code example, just mentions of avoiding unnecessary whitespace,
+# In the realm of purity, let code interlace.
 
 # Other Recommendations
-x = 5  # Spaces keep order,
-        # Tokens aligned, harmony's border,
-        # Gaps, where beauty can wander.
+x = 5  # Spaces, order it creates,
+        # Tokens aligned, harmony permeates,
+        # Serenity, where clarity emanates.
 
 y = some_function(argument1, argument2)
 
@@ -102,66 +104,68 @@ z = another_function(argument1, argument2)
 # Comments
 
 # Inline Comments
-x = x + 1  # Increment x, a wise notation,
-            # Change's whispers, no deviation,
-            # Clarity, in each translation.
+x = x + 1  # Incrementing x, a silent decree,
+            # Comment's wisdom, for all to see,
+            # Illuminating code, setting thoughts free.
 
-y = y - 1  # Decrement y, reader's key,
-            # Comment's magic, new eyes to see,
-            # Code's story, clear and free.
+y = y - 1  # Decrementing y, a step back we take,
+            # Comments guiding, like a sacred lake,
+            # Code's whispers, truth it will make.
 
 # Block Comments
 """
-Whispered tales untold,
-In code's crevices, they unfold,
-Guiding lost souls, bold.
+Whispers of knowledge, secrets untold,
+Comments unveil, in wisdom's stronghold,
+Understanding awakens, as mysteries unfold.
 """
 x = 5
 y = 10
 
 """
-Inner secrets shared,
-Code's masterpiece declared,
-Wisdom unimpaired.
+Hidden treasures, words of insight,
+Block comments, shining light so bright,
+Transparency revealed, in pure code's flight.
 """
 z = x + y
 
 # Documentation Strings
 def my_function():
     """
-    Seekers find solace,
-    Docstrings illuminate, grace,
-    Secrets revealed, embrace.
+    Seekers of truth, gather 'round,
+    Docstrings enlighten, in silence profound,
+    Unlocking code's mysteries, wisdom unbound.
     """
 
 def another_function():
     """
-    Enigmatic script,
-    Docstrings, clear and adept,
-    Clarity, never inept.
+    Code's enigma, a story untold,
+    Docstrings reveal, in language bold,
+    Clarity unleashed, as secrets unfold.
     """
 
 # Naming Conventions
 
 # Overriding Principle
-# No specific code example, a reminder to choose names with clarity, a vital essence.
+# No specific code example, just a reminder, names to be wise,
+# Conveying meaning, truth they personify.
 
 # Descriptive: Naming Styles
-# No specific code example, meaningful names, the heart's mile.
+# No specific code example, meaningful names, profound and deep,
+# Code's language, where understanding shall seep.
 
 # Prescriptive: Naming Conventions
 
 ## Names to Avoid
-# No specific code example, caution's plea,
-# Obscurity's ban, clarity's decree.
+# No specific code example, a cautionary decree,
+# Obscure names, clarity we decree.
 
 ## ASCII Compatibility
-# No specific code example, embrace ASCII's grace,
-# Universal harmony, code's vibrant embrace.
+# No specific code example, embracing ASCII's soul,
+# Universal harmony, where coding's truth shall unroll.
 
 ## Package and Module Names
-# No specific code example, lowercase with underscore's bliss,
-# Forging paths, code's eternal kiss.
+# No specific code example, lowercase with humble grace,
+# Forging paths, a tranquil embrace.
 
 ## Class Names
 class MyClass:
@@ -174,14 +178,14 @@ class AnotherClass:
 try:
     # code that may raise an exception
 except ValueError as e:
-    # Exception named, its power tamed,
-    # With understanding, chaos tamed.
+    # Named exception, its essence portrayed,
+    # Understanding chaos, with clarity conveyed.
 
 try:
     # code that may raise another exception
 except KeyError as e:
-    # Another exception subdued,
-    # Named, its identity renewed.
+    # Another exception, named to ignite,
+    # Its identity revealed, in code's sacred light.
 
 ## Global Variable Names
 GLOBAL_CONSTANT = 42
@@ -204,68 +208,68 @@ def another_function():
 # Default Argument Values
 def my_function(arg1, arg2=None):
     """
-    Default paths unfold,
-    Flexibility, code's stronghold,
-    Harmony, stories untold.
+    Default paths, flexibility unfurled,
+    Embracing choices, a serene world,
+    Versatility, where code's wisdom is hurled.
     """
     if arg2 is None:
         arg2 = []
 
 def another_function(arg1, arg2=True, arg3=10):
     """
-    A world of choices,
-    Default values, diverse voices,
-    Versatility rejoices.
+    A realm of possibilities, options abound,
+    Default values, where power is found,
+    Embracing adaptability, code's peace shall resound.
     """
     pass
 
 # Keyword Arguments and Positional Arguments
 def my_function(arg1, arg2, *args, **kwargs):
     """
-    Arguments in dance,
-    Positional and keywords, code's romance,
-    Harmony in every chance.
+    Arguments in harmony, a gentle sway,
+    Positional and keywords, in unity they play,
+    Code's serenade, where insights lay.
     """
     pass
 
 def another_function(arg1, arg2, *args, **kwargs):
     """
-    A symphony, united calls,
-    Positional and keywords, as the light falls,
-    Flowing river, code's grand halls.
+    A symphony of calls, where unity thrives,
+    Positional and keywords, code's truth arrives,
+    A flowing river, where harmony derives.
     """
     pass
 
 # Function and Method Decorators
-# No specific code example, decorators' realm,
-# Magic bestowed, code's sacred helm.
+# No specific code example, a domain where decorators weave,
+# Enchanting magic, where code's essence does conceive.
 
 # Intermezzo: Coding Style
-# No specific code example, a moment to pause,
-# Reflect on code's grandiose applause.
+# No specific code example, a moment of reflection, a serene abode,
+# Harmonizing style and substance, where peace is bestowed.
 
 ## Guidelines & Best Practices
 
 ### Use of the Backslash
 my_string = (
-    "Lines gracefully flow, "
-    "Backslash binds, code's enchanting glow, "
-    "Verse-like beauty, a rhythm to bestow."
-    )
+    "Lines gracefully flow, in perfect accord, "
+    "Backslash unites, their spirits soared, "
+    "Poetry of code, where beauty is restored."
+)
 
 another_string = (
-    "Lines connected, like a pathway defined, "
-    "Backslash, the bridge, forever entwined, "
-    "A story unfolds, a tale for the mind."
-    )
+    "Lines entwined, a path divine, "
+    "Backslash, a bridge, where thoughts align, "
+    "A journey of code, where secrets intertwine."
+)
 
 ### When to Use Trailing Commas
 my_list = [
     "item1",
     "item2",
-    "item3",  # Trailing comma's gentle touch,
-                # Readability's flourish, it means so much,
-                # Growth, painless and such.
+    "item3",  # Trailing comma, an elegant pause,
+              # Readability heightened, without a cause,
+              # Growth and flexibility, a harmonious applause.
 ]
 
 another_list = [
@@ -306,15 +310,15 @@ class AnotherClass:
 
 # Conclusion
 """
-Embrace PEP8's grace,
-Code, an art form's embrace,
-Beauty in each trace.
+Embrace PEP8's prophecy,
+Code's artistry, creating harmony,
+In simplicity, the code shall be free.
 """
 
 # References
 """
-Seek wisdom's sacred well,
-Sages' guidance, let it dwell,
-Code's journey, onward, swell.
+Seek wisdom's ancient scrolls,
+Sages' guidance, where knowledge unfolds,
+Code's journey, where serenity beholds.
 """
 
